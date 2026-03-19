0e079ed3c28986e9c9434bd1d1792cb7501cca07
Sandro Tvaliashvili <sandrotvaliashvili10@gmail.com>
2026-03-19 07:31:51 +0400

    supabase url fix

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
 export const supabase = createClient(
-  'https://fobmuromfiznouecrmlg.supabase.co',
+  'https://fobnuromfiznouecmrlg.supabase.co',
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZvYm51cm9tZml6bm91ZWNtcmxnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM4NTAxOTIsImV4cCI6MjA4OTQyNjE5Mn0.DEJY9bj3vxbjV4NKx7wncmTGlRdGXuCfJUHfDFFvZyo'
 );
