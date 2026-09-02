7ff0c92c2fb4fd4d693646b28a470b289de04946
Sandro Tvaliashvili <sandrotvaliashvili10@gmail.com>
2026-09-02 12:11:13 +0400

    start over

--- firebase-config.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
-
-export const supabase = createClient(
-  'https://your-project.supabase.co',
-  'your-anon-key'
-)
